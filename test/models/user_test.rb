c259a3326d671958c501c91536f5f13c06be7f90
uyah <yuya.uwatoko@doorkel.com>
2019-02-05 16:59:07 +0900

    added sign up forms/chapter 7.2.1

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -72,5 +72,12 @@ class UserTest < ActiveSupport::TestCase
     @user.password = @user.password_confirmation = "a" * 5
     assert_not @user.valid?
   end
+
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
   
 end
