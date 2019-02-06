f51387653858e2e0076e1c9164289ec866275c5b
uyah <yuya.uwatoko@doorkel.com>
2019-02-06 16:05:52 +0900

    Add signup forms and error messages: Chapter 7.3

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -72,12 +72,5 @@ class UserTest < ActiveSupport::TestCase
     @user.password = @user.password_confirmation = "a" * 5
     assert_not @user.valid?
   end
-
-  test "email addresses should be unique" do
-    duplicate_user = @user.dup
-    duplicate_user.email = @user.email.upcase
-    @user.save
-    assert_not duplicate_user.valid?
-  end
   
 end
